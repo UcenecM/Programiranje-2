6a7f0e63f0af9943f3622d7c001ccc985dad0f36
Mark Buda <147140831+UcenecM@users.noreply.github.com>
2025-10-06 10:15:17 +0200

    Add files via upload

--- Urejanje/Urejanje/Program.cs
+++ Urejanje/Urejanje/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Tabela a = new Tabela(10);
+            //a.Dodaj(6); a.Dodaj(24); a.Dodaj(80);
+            //a.Dodaj(4); a.Dodaj(19); a.Dodaj(84);
+            //a.Dodaj(1); a.Dodaj(10); a.Dodaj(13); a.Dodaj(7);
+
+
+            Random r = new Random();
+            Tabela a = new Tabela(11);
+            for(int k=0; k<100; k++)
+            {
+
+            }
+
+            Console.WriteLine("NEurejena tabela");
+            a.Izpiši();
+            a.QuickSort(0, 9);
+            Console.WriteLine("URejenja tablea");
+            a.Izpiši();
         }
     }
 }
--- Urejanje/Urejanje/Tabela.cs
+++ Urejanje/Urejanje/Tabela.cs
@@ -24,9 +24,56 @@ namespace Urejanje
         {
             for (int k = 0; k<tab.Length; k++)
             {
-                Console.WriteLine("{0,4", tab[k]);
+                Console.Write("{0,4}", tab[k]);
             }
+
             Console.WriteLine();
+        }
+        private int Pivot(int zač, int konec)
+        {
+            //metoda vrne pozicijo pivotnega elementa
+            int p = tab[zač];
+            int m = zač;
+            int n = konec + 1;
+            //z m poišči prvega, ki je večji od p
+            do
+            {
+                m = m + 1;
+            } while (tab[m] <= p && m < konec);
+            //z n išči prveg, ki je manjši od p
+            do
+            {
+                n = n - 1;
+            } while (tab[n] > p);
+            //dokler se m in n ne prekrižata
+            while (m < n)
+            {
+                //zamenjaj m-ti in n-ti element
+                int temp = tab[m]; tab[m] = tab[n]; tab[n] = temp;
+                //z m poišči prvega, ki je večji od p
+                do
+                {
+                    m = m + 1;
+                } while (tab[m] <= p && m < konec);
+                //z n išči prveg, ki je manjši od p
+                do
+                {
+                    n = n - 1;
+                } while (tab[n] > p);
+            }
+            int temp1 = tab[zač]; tab[zač] = tab[n]; tab[n] = temp1;
+            return n;
+        }
+        public void QuickSort(int zač, int konec)
+        {
+            if(zač >= konec)
+            {
+                return;
+            }
+            int delitev = Pivot(zač, konec);
+            Izpiši();
+            QuickSort(zač, delitev - 1);
+            QuickSort(delitev + 1, konec);
         }
     }
 }
