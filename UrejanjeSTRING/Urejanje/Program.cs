53f0512ece45d515ed8664223eeab124f573494e
Mark Buda <147140831+UcenecM@users.noreply.github.com>
2025-10-06 10:47:01 +0200

    Add files via upload

---
+++ UrejanjeSTRING/Urejanje/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Urejanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabela a = new Tabela(10);
+            a.Dodaj("Miha"); a.Dodaj("Ana"); a.Dodaj("Barbara");
+            a.Dodaj("Vid"); a.Dodaj("Jan"); a.Dodaj("Žan");
+            a.Dodaj("Lia"); a.Dodaj("Žiga"); a.Dodaj("Liam"); a.Dodaj("Tone");
+
+            Console.WriteLine("NEurejena tabela");
+            a.Izpiši();
+            a.QuickSort(0, 9);
+            Console.WriteLine("URejenja tablea");
+            a.Izpiši();
+        }
+    }
+}
---
+++ UrejanjeSTRING/Urejanje/Tabela.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Urejanje
+{
+    internal class Tabela
+    {
+        string[] tab;
+        int štElementov;
+
+        public Tabela(int velikost)
+        {
+            tab = new string[velikost];
+            štElementov = 0;
+        }
+
+        public void Dodaj(string k)
+        {
+            tab[štElementov++] = k;
+        }
+
+        public void Izpiši()
+        {
+            for (int k = 0; k < tab.Length; k++)
+            {
+                Console.Write(tab[k] + " ");  
+            }
+
+            Console.WriteLine();
+        }
+
+        private int Pivot(int zač, int konec)
+        {
+            string p = tab[zač];
+            int m = zač;
+            int n = konec + 1;
+
+            do
+            {
+                m = m + 1;
+            } while (string.Compare(tab[m], p) <= 0 && m < konec);
+
+            do
+            {
+                n = n - 1;
+            } while (string.Compare(tab[n], p) > 0);
+
+            while (m < n)
+            {
+                string temp = tab[m];
+                tab[m] = tab[n];
+                tab[n] = temp;
+
+                do
+                {
+                    m = m + 1;
+                } while (string.Compare(tab[m], p) <= 0 && m < konec);
+
+                do
+                {
+                    n = n - 1;
+                } while (string.Compare(tab[n], p) > 0);
+            }
+
+            string temp1 = tab[zač];
+            tab[zač] = tab[n];
+            tab[n] = temp1;
+            return n;
+        }
+
+        public void QuickSort(int zač, int konec)
+        {
+            if (zač >= konec)
+            {
+                return;
+            }
+            int delitev = Pivot(zač, konec);
+            QuickSort(zač, delitev - 1);
+            QuickSort(delitev + 1, konec);
+        }
+    }
+}
