d25d87f70e365ed90b67aa75346aa847b55abd4e
Mark Buda <147140831+UcenecM@users.noreply.github.com>
2025-10-09 12:36:26 +0200

    Add files via upload

---
+++ KliciMe/KliciMe/Kupec.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace KliciMe
+{
+    internal class Kupec
+    {
+        //enkapsulacija neki neki
+        private string ime;
+        private decimal stanje;
+
+        public string Ime { get => ime; set => ime = value; }
+        public decimal Stanje { get => stanje; set => stanje = value; }
+
+        public void BeležiStanje(decimal plačilo)
+        {
+            stanje -= plačilo;
+        }
+        public void BeležiKlic(int minute, int tipKlica)
+        {
+            //1-stacionarno=minute*0.2,2- mobilno=minute*0.03
+            switch(tipKlica){
+                case 1:
+                    stanje += minute * 0.2m;
+                    break;
+                case 2:
+                    stanje += minute * 0.3m;
+                    break;
+            }
+        }
+    }
+}
---
+++ KliciMe/KliciMe/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace KliciMe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kupec Janez = new Kupec();
+            Janez.Ime = "Janez";
+            Janez.BeležiKlic(10, 1);
+            Console.WriteLine(Janez.Ime+" dolguuje " + Janez.Stanje + " EUR");
+        }
+    }
+}
