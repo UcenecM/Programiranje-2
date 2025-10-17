cb641f7b840a2e0108000addcbc244588903fa49
Mark Buda <147140831+UcenecM@users.noreply.github.com>
2025-10-17 10:55:45 +0200

    Add files via upload

---
+++ Preobteževanje operatorjev/Preobteževanje operatorjev/Pravokotnik.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preobteževanje_operatorjev
+{
+    internal class Pravokotnik
+    {
+        public double širina;
+        public double višina;
+
+        public Pravokotnik()
+        {
+            širina = 0;
+            višina = 0;
+        }
+
+        public Pravokotnik(double širina, double višina)
+        {
+            this.širina = širina;
+            this.višina = višina;
+        }
+
+        public double Širina
+        {
+            get { return širina; }
+            set { širina = value; }
+        }
+
+        public double Višina
+        {
+            get { return višina; }
+            set { višina = value; }
+        }
+
+        public double Ploščina
+        {
+            get { return širina * višina; }
+        }
+
+        //operator ==
+        public static bool operator ==(Pravokotnik p1, Pravokotnik p2)
+        {
+            if (ReferenceEquals(p1, null) && ReferenceEquals(p2, null))
+                return true;
+            if (ReferenceEquals(p1, null) || ReferenceEquals(p2, null))
+                return false;
+
+            return p1.Ploščina == p2.Ploščina;
+        }
+
+  
+        //Operator !=
+        public static bool operator !=(Pravokotnik p1, Pravokotnik p2)
+        {
+            return !(p1 == p2);
+        }
+
+
+        //Operator >
+        public static bool operator >(Pravokotnik p1, Pravokotnik p2)
+        {
+            if (ReferenceEquals(p1, null))
+                return false;  
+            if (ReferenceEquals(p2, null))
+                return true;  
+
+            return p1.Ploščina > p2.Ploščina;
+        }
+
+        //Operator <
+        public static bool operator <(Pravokotnik p1, Pravokotnik p2)
+        {
+            if (ReferenceEquals(p1, null))
+                return !ReferenceEquals(p2, null);
+            if (ReferenceEquals(p2, null))
+                return false;
+
+            return p1.Ploščina < p2.Ploščina;
+        }
+        //Operator >=
+        public static bool operator >=(Pravokotnik p1, Pravokotnik p2)
+        {
+            return (p1 > p2) || (p1 == p2);
+        }
+
+        //Operator <=
+        public static bool operator <=(Pravokotnik p1, Pravokotnik p2)
+        {
+            return (p1 < p2) || (p1 == p2);
+        }
+        public override string ToString()
+        {
+            return $"Višina = {višina}, Širina = {širina}";
+        }
+
+    }
+
+}
---
+++ Preobteževanje operatorjev/Preobteževanje operatorjev/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Preobteževanje_operatorjev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pravokotnik p1 = new Pravokotnik();
+            Pravokotnik p2 = new Pravokotnik();
+            Pravokotnik p3 = new Pravokotnik(10, 15);
+            Pravokotnik p4 = new Pravokotnik(20, 30);
+            p1.Širina = 10;
+            p1.Višina = 15;
+            p2.Širina = 25; p2.Višina = 10;
+            Console.WriteLine("Pravokotnik 1 " + p1.ToString());
+            Console.WriteLine("Pravokotnik 1 " + p2.ToString());
+            Console.WriteLine("Pravokotnik 1 " + p3.ToString());
+            if (p1 == p2) Console.WriteLine("p1=p2");
+            else
+            {
+                if (p1 > p2) Console.WriteLine("P1 je večji od P2");
+                else Console.WriteLine("P2 je večji od P1");
+            }
+            if (p1 == p3) Console.WriteLine("P1 je enak P3");
+            Console.ReadLine();
+        }
+    }
+}
