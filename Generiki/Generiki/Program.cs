ddf530342244c7ff854c82404c432c06eb871cfb
Mark Buda <147140831+UcenecM@users.noreply.github.com>
2025-10-20 11:43:43 +0200

    Add files via upload

---
+++ Generiki/Generiki/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace Generiki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Push(1);
+            s.Push(2);
+            s.Push("A");
+            int x = (int)s.Pop();
+            Console.WriteLine(x);
+            x = (int)s1.Pop();
+            Console.WriteLine(x);
+            SkladGen<int> s2= new SkladGen<int>
+        }
+    }
+}
---
+++ Generiki/Generiki/sklad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generiki
+{
+    internal class sklad
+    {
+        int m_size;
+        int m_Items;
+        public sklad(int velikost)
+        {
+            m_size = velikost;
+            m_Items = new object[m_size];
+        }
+
+    }
+}
